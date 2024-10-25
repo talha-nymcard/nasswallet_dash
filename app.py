24caa5ebd5a4059f277a3ef73c1963aa61940fe1
talha-nymcard <muhammad.talha@nymcard.com>
2024-10-25 20:03:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 import streamlit as st
-
 from metrics_display import display_metrics
-
 from transaction_metrics import display_transaction_metrics  # Import transaction metrics
-
 # Set page configuration
 st.set_page_config(page_title="Nasswallet Dashboard", layout="wide")
+# Inject JavaScript to force dark theme
+dark_theme_js = """
+<script>
+    const setDarkMode = () => {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    };
+    window.addEventListener('load', setDarkMode);
+</script>
+"""
+# Inject the CSS and JavaScript into the Streamlit app
+
+st.markdown(dark_theme_js, unsafe_allow_html=True)
+# Set page configuration
+
 
 # Title of the application
-st.title("NassPay Dashboard")
+st.title("Nasswallet Dashboard")
 
 # Create columns to display Cardholder and Card metrics side by side
 #col1, col2 = st.columns(2)
@@ -24,5 +35,6 @@ display_metrics()
 # This one looks good on bigger screen but the text alignment isn't ok for smaller screensize.
 #display_metrics1()
 # Create a new row for Transaction Metrics
-st.subheader("Transaction Metrics")
+st.subheader("Transaction Summary")
+st.markdown("<hr>", unsafe_allow_html=True)
 display_transaction_metrics()  # Call the function to display transaction metrics
